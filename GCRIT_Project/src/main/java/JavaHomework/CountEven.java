1edcd52b2678cab27248e728933e850e3931c981
jessy <jessy@DESKTOP-ILDCDQF>
2018-06-29 19:20:03 -0400

    Homework 06/29

---
+++ GCRIT_Project/src/main/java/JavaHomework/CountEven.java
@@ -0,0 +1,18 @@
+package JavaHomework;
+
+public class CountEven {
+
+	public static void main(String[] args) {
+
+		int count =0;
+		
+		while(count <=100) {
+		System.out.println(count  +  "   This is even number");
+		
+		count=count+2;
+		
+		}
+
+	}
+
+}
---
+++ GCRIT_Project/src/main/java/JavaHomework/CountOdd.java
@@ -0,0 +1,16 @@
+package JavaHomework;
+
+public class CountOdd {
+
+	public static void main(String[] args) {
+		int count =1;
+		
+		while(count <=100) {
+		System.out.println(count  +  "   This is odd number");
+		
+		count=count+2;
+		
+		}
+	}
+}
+
